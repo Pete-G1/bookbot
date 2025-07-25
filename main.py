123e1060f8657771c71b267ed327e9fd3f838d55
Pete-G1 <petegarcia437@gmail.com>
2025-07-25 01:03:12 -0500

    Added character count sorting and reporting functionality. Added explanation comments.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_word_count
 from stats import get_char_count
+from stats import sort_report
 
 def get_book_text(r):
     with open(r) as f:
@@ -8,10 +9,20 @@ def get_book_text(r):
 
 def main():
     test = get_book_text("books/frankenstein.txt")
-    get_word_count(test)
     characters = get_char_count(test)
-    print(characters)
-    return test
+    dic = sort_report(characters)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    get_word_count(test)
+    print("--------- Character Count -------")
+    for char in dic:
+        item = char["char"]
+        if item.isalpha() == True:
+            print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")        
+      
+
 
 main()    
 
--- stats.py
+++ stats.py
@@ -3,7 +3,8 @@
 def get_word_count(test):
     count = test.split()
     word_count = len(count)
-    print(f"{word_count} words found in the document")
+    print(f"Found {word_count} total words")
+#counts total words in book. 
 
 def get_char_count(test):
     char_count = {
@@ -17,8 +18,27 @@ def get_char_count(test):
             char_count[letter] = 1
 
     return char_count
+#returns a dictionary of characters and how often they appear.
+def sort_on(items):
+    return items["num"]  
+ #helper function for sort_report 
 
-  
+def sort_report(report_data):
+    report_list = []
+    for char, num in report_data.items():
+        temp = {"char":char, "num": num}
+        report_list.append(temp)
+    report_list.sort(reverse=True, key=sort_on)
+    return report_list
+ #turns dictionary of characters and their counts into a list of dictionaries of characters and their counts.    
+        
+    
+
+
+
+
+    
+
 
 
 
